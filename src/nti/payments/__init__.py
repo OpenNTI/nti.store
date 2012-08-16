124aae50f320d94bfe07250a8e6455ad2b692c5e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-08-16 00:26:57 +0000

    add some constants
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@10155 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/payments/__init__.py
+++ src/nti/payments/__init__.py
@@ -1,4 +1,9 @@
 from __future__ import print_function, unicode_literals
 
-status_codes = (u'Reserved', u'Reserved', u'Success', u'Failure', u'Failed',
-                u'Pending', u'Cancelled')
+FAILED = u'Failed'
+PENDING = u'Pending'
+SUCCESS = u'Success'
+CANCELLED = 'Cancelled'
+
+status_codes = (u'Reserved', u'Reserved', SUCCESS, u'Failure', FAILED,
+                PENDING, CANCELLED)
