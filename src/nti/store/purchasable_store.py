546f740f83537ddd80589be88d074a0732460c29
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-04-04 01:32:45 +0000

    add helper methods
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@17899 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/store/purchasable_store.py
+++ src/nti/store/purchasable_store.py
@@ -1,22 +1,36 @@
 # -*- coding: utf-8 -*-
 """
-Defines Purchasable Store.
+Purchasable store helper methods
 
 $Id$
 """
 from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
 
+from zope import component
+
 from nti.dataserver.users import User
 from nti.dataserver import interfaces as nti_interfaces
 
 from . import interfaces as store_interfaces
 
+def get_purchasables():
+	utils = component.getUtilitiesFor(store_interfaces.IPurchasable)
+	result = {k:v for  k, v in utils}
+	return result
+
 def get_available_items(store, user):
+	# gather all item that can be purchased
+	purchasables = get_purchasables()
+	all_ids = set(purchasables.keys())
+
+	# get purchase history
 	user = User.get_user(str(user)) if not nti_interfaces.IUser.providedBy(user) else user
 	history = store_interfaces.IPurchaseHistory(user)
-	all_ids = set(store.keys())
 	purchased = set()
 	for p in history:
 		purchased.update(p.Items)
-	return all_ids - purchased
+
+	available = all_ids - purchased
+	result = {k:purchasables.get(k) for k in available}
+	return result
