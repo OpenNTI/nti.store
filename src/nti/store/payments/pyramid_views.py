87c72f08144330dca573d149ac4da8d57f20eb37
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-04-12 17:33:48 +0000

    refactor a bit the stripe pyramid views
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@18206 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/store/payments/pyramid_views.py
+++ src/nti/store/payments/pyramid_views.py
@@ -7,114 +7,24 @@ $Id$
 from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
 
-logger = __import__('logging').getLogger(__name__)
-
-import gevent
-
-from zope import component
-
-from pyramid import httpexceptions as hexc
-from pyramid.security import authenticated_userid
-
-from nti.externalization.datastructures import LocatedExternalDict
-
-from .. import purchase_attempt
-from .. import purchase_history
-from .. import interfaces as store_interfaces
 from .stripe import pyramid_views as stripe_pyramid
 
-def _valid_amount(amount):
+def is_valid_amount(amount):
 	try:
 		amount = float(amount)
 		return amount > 0
 	except:
 		return False
 
-def _valid_pve_int(value):
+def is_valid_pve_int(value):
 	try:
 		value = float(value)
 		return value > 0
 	except:
 		return False
 
-class BasePaymentView(object):
-
-	processor = None
-
-	def __init__(self, request):
-		self.request = request
-
-	def __call__(self):
-		request = self.request
-		username = authenticated_userid(request)
-		items = request.params.get('items', request.params.get('nttid', None))
-
-		# check for any pending purchase for the items being bought
-		purchase = purchase_history.get_pending_purchase_for(username, items)
-		if purchase is not None:
-			logger.warn("There is already a pending purchase for item(s) %s" % items)
-			return purchase
-
-		token = request.params.get('token', None)
-		amount = request.params.get('amount', None)
-		if not token or not _valid_amount(amount):
-			raise hexc.HTTPBadRequest()
-
-		coupon = request.params.get('coupon', None)
-		currency = request.params.get('currency', 'USD')
-		description = request.params.get('description', None)
-
-		quantity = request.params.get('quantity', None)
-		if quantity and not _valid_pve_int(quantity):
-			raise hexc.HTTPBadRequest()
-
-		description = description or "%s's payment for '%r'" % (username, items)
-
-		purchase = purchase_attempt.create_purchase_attempt(items=items, processor=self.processor,
-															description=description, quantity=quantity)
-		purchase_id = purchase_history.register_purchase_attempt(username, purchase)
-
-		def process_pay():
-			manager = component.getUtility(store_interfaces.IPaymentProcessor, name=self.processor)
-			manager.process_purchase(purchase_id=purchase_id, username=username, token=token, amount=amount,
-									 currency=currency, coupon=coupon)
-		gevent.spawn(process_pay)
-		return purchase
-
-class StripePaymentView(BasePaymentView):
-	processor = 'stripe'
-
-class BaseValidateCouponView(object):
-	processor = None
-
-	def __init__(self, request):
-		self.request = request
-
-	def __call__(self):
-		request = self.request
-		manager = component.getUtility(store_interfaces.IPaymentProcessor, name=self.processor)
-		coupon = request.params.get('coupon', request.params.get('code', None))
-		amount = request.params.get('amount')
-		if amount is not None and not _valid_amount(amount):
-			raise hexc.HTTPBadRequest()
-
-		# stripe defines an 80 sec timeout for http requests
-		# at this moment we are to wait for coupon validation
-		validated_coupon = manager.validate_coupon(coupon)
-		if validated_coupon:
-			result = LocatedExternalDict()
-			if amount:
-				amount = manager.apply_coupon(amount, validated_coupon)
-				result['Amount'] = amount
-			result['Coupon'] = coupon
-			return result
-		else:
-			raise hexc.HTTPNotFound()
-
-class ValidateStripeCouponView(BaseValidateCouponView):
-	processor = 'stripe'
-
 # alias
 
+StripePaymentView = stripe_pyramid.StripePaymentView
 GetStripeConnectKeyView = stripe_pyramid.GetStripeConnectKeyView
-
+ValidateStripeCouponView = stripe_pyramid.ValidateStripeCouponView
--- src/nti/store/payments/stripe/pyramid_views.py
+++ src/nti/store/payments/stripe/pyramid_views.py
@@ -7,21 +7,101 @@ $Id$
 from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
 
+logger = __import__('logging').getLogger(__name__)
+
+import gevent
+
 from zope import component
 
 from pyramid import httpexceptions as hexc
+from pyramid.security import authenticated_userid
 
+from nti.externalization.datastructures import LocatedExternalDict
+
+from ... import purchase_attempt
+from ... import purchase_history
 from . import interfaces as stripe_interfaces
+from ... import interfaces as store_interfaces
+from ..pyramid_views import is_valid_amount, is_valid_pve_int
 
 class GetStripeConnectKeyView(object):
+	processor = 'stripe'
 
 	def __init__(self, request):
 		self.request = request
 
 	def __call__(self):
-		prms = self.request.params
-		keyname = prms.get('alias', prms.get('name', u''))
+		params = self.request.params
+		keyname = params.get('provider', params.get('alias', u''))
 		result = component.queryUtility(stripe_interfaces.IStripeConnectKey, keyname)
 		if result is None:
-			raise hexc.HTTPNotFound()
+			raise hexc.HTTPNotFound(detail='Provider not found')
 		return result
+
+class ValidateStripeCouponView(GetStripeConnectKeyView):
+
+	def __call__(self):
+		stripe_key = super(ValidateStripeCouponView, self).__call__()
+
+		request = self.request
+		coupon = request.params.get('coupon', request.params.get('code', None))
+		amount = request.params.get('amount')
+		if amount is not None and not is_valid_amount(amount):
+			raise hexc.HTTPBadRequest(detail='invalid amount')
+
+		manager = component.getUtility(store_interfaces.IPaymentProcessor, name=self.processor)
+
+		# stripe defines an 80 sec timeout for http requests
+		# at this moment we are to wait for coupon validation
+		coupon = manager.get_coupon(coupon, api_key=stripe_key.PrivateKey)
+		validated_coupon = manager.validate_coupon(coupon, api_key=stripe_key.PrivateKey) if coupon is not None else False
+		if validated_coupon:
+			result = LocatedExternalDict()
+			if amount:
+				amount = manager.apply_coupon(float(amount), coupon)
+				result['Amount'] = amount
+			result['Coupon'] = coupon
+			return result
+		else:
+			raise hexc.HTTPNotFound(detail="Invalid coupon")
+
+class StripePaymentView(GetStripeConnectKeyView):
+
+	def __call__(self):
+		stripe_key = super(StripePaymentView, self).__call__()
+
+		request = self.request
+		username = authenticated_userid(request)
+		items = request.params.get('items', request.params.get('nttid', None))
+
+		# check for any pending purchase for the items being bought
+		purchase = purchase_history.get_pending_purchase_for(username, items)
+		if purchase is not None:
+			logger.warn("There is already a pending purchase for item(s) %s" % items)
+			return purchase
+
+		token = request.params.get('token', None)
+		amount = request.params.get('amount', None)
+		if not token or not is_valid_amount(amount):
+			raise hexc.HTTPBadRequest(detail="Invalid amount")
+
+		coupon = request.params.get('coupon', None)
+		currency = request.params.get('currency', 'USD')
+		description = request.params.get('description', None)
+
+		quantity = request.params.get('quantity', None)
+		if quantity and not is_valid_pve_int(quantity):
+			raise hexc.HTTPBadRequest()
+
+		description = description or "%s's payment for '%r'" % (username, items)
+
+		purchase = purchase_attempt.create_purchase_attempt(items=items, processor=self.processor,
+															description=description, quantity=quantity)
+		purchase_id = purchase_history.register_purchase_attempt(username, purchase)
+
+		def process_pay():
+			manager = component.getUtility(store_interfaces.IPaymentProcessor, name=self.processor)
+			manager.process_purchase(purchase_id=purchase_id, username=username, token=token, amount=amount,
+									 currency=currency, coupon=coupon, api_key=stripe_key.PrivateKey)
+		gevent.spawn(process_pay)
+		return purchase
