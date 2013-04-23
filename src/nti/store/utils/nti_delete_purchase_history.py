1a07b8a080522cdea0ae95f108ff672a1741e22f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-04-23 15:00:41 +0000

    simple view to delete a purchase history. This should go to app views
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@18562 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/store/utils/nti_delete_purchase_history.py
+++ src/nti/store/utils/nti_delete_purchase_history.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Delete a purchase attempt 
+Delete a purchase history 
 
-$Id: nti_reindex_entity_content.py 17262 2013-03-18 17:20:50Z carlos.sanchez $
+$Id$
 """
 from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
@@ -12,43 +12,49 @@ import os
 import sys
 import argparse
 
+from zope import lifecycleevent
+from zope.annotation import IAnnotations
+
 from nti.dataserver import users
 from nti.dataserver.utils import run_with_dataserver
 
 import nti.store
-from .. import purchase_history
+from .. import interfaces
 
-def _delete_purchase(username, purchase_id, verbose=False):
+def _delete_history(username, purchase_id, verbose=False):
 	user = users.User.get_user(username)
 	if not user:
 		print("User '%s' does not exists" % username, file=sys.stderr)
 		sys.exit(2)
 
-	purchase = purchase_history.get_purchase_attempt(purchase_id, user)
-	if not purchase:
-		print("Purchase attempt '%s' does not exists" % purchase_id, file=sys.stderr)
-		sys.exit(2)
+	su = interfaces.IPurchaseHistory(user)
 
-	purchase_history.remove_purchase_attempt(purchase, user)
+	# bwc
+	if hasattr(su, "purchases"):
+		for p in su.purchases.values():
+			lifecycleevent.removed(p)
+	elif hasattr(su, "clear"):
+		su.clear()
+
+	IAnnotations(user).pop("%s.%s" % (su.__class__.__module__, su.__class__.__name__), None)
+
 	if verbose:
-		print("Purchase attempt '%s' has been removed" % purchase_id)
+		print("Purchase history has been removed")
 
 def main():
-	arg_parser = argparse.ArgumentParser(description="Delete a purchase attempt")
+	arg_parser = argparse.ArgumentParser(description="Delete user's purchase history")
 	arg_parser.add_argument('env_dir', help="Dataserver environment root directory")
 	arg_parser.add_argument('username', help="The username")
-	arg_parser.add_argument('purchase_id', help="The purchase attempt ID")
 	arg_parser.add_argument('-v', '--verbose', help="Verbose output", action='store_true', dest='verbose')
 	args = arg_parser.parse_args()
 
 	verbose = args.verbose
 	username = args.username
-	purchase_id = args.purchase_id
 	env_dir = os.path.expanduser(args.env_dir)
 
 	run_with_dataserver(environment_dir=env_dir,
 						xmlconfig_packages=(nti.store,),
-						function=lambda: _delete_purchase(username, purchase_id, verbose))
+						function=lambda: _delete_history(username, verbose))
 
 if __name__ == '__main__':
 	main()
