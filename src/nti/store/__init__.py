2e66b3543aa468394d07867ab4c1574f701d7134
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-09-30 23:46:58 +0000

    break dep w. nti.appserver site policies
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@25000 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/store/__init__.py
+++ src/nti/store/__init__.py
@@ -13,6 +13,8 @@ MessageFactory = zope.i18nmessageid.MessageFactory('nti.dataserver')
 
 from zope import interface
 
+from pyramid.threadlocal import get_current_request
+
 from .utils import *
 from . import interfaces as store_interfaces
 
@@ -25,3 +27,14 @@ class InvalidPurchasable(NTIStoreException):
 
 class PricingException(NTIStoreException):
     pass
+
+def get_possible_site_names(request=None, include_default=False):
+    request = request or get_current_request()
+    if not request:
+        return () if not include_default else ('',)
+    __traceback_info__ = request
+
+    site_names = getattr(request, 'possible_site_names', ())
+    if include_default:
+        site_names += ('',)
+    return site_names
