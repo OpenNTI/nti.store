d041149ca31c8575e01269958ee81c640cfc400e
Jason Madden <jason.madden@nextthought.com>
2013-12-21 21:27:23 +0000

    use z3c.schema fields for CC and email validation.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@28461 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/store/payments/utils.py
+++ src/nti/store/payments/utils.py
@@ -10,43 +10,39 @@ __docformat__ = "restructuredtext en"
 import re
 
 from zope import schema
+from z3c.schema.payments import ISO7812CreditCard
+from z3c.schema.payments import isValidCreditCard
+from z3c.schema.payments import NotValidISO7812CreditCard
+_cc = ISO7812CreditCard()
+import numbers
 
 def is_valid_creditcard_number(s):
 	"""
 	Returns True if the credit card number ``s`` is valid, False otherwise.
-	
-	http://code.activestate.com/recipes/577838-credit-card-validation/
-	http://atlee.ca/blog/2008/05/27/validating-credit-card-numbers-in-python/
-	"""
-	s = re.sub("[^0-9]", "", str(s))
-	regexps = [
-			"^4\d{15}$",
-			"^5[1-5]\d{14}$",
-			"^3[4,7]\d{13}$",
-			"^3[0,6,8]\d{12}$",
-			"^6011\d{12}$",
-	        ]
 
-	if not any(re.match(r, s) for r in regexps):
+	"""
+
+	if isinstance(s, numbers.Integral):
+		s = str(s)
+	elif isinstance(s, basestring):
+		# Drop spaces and dashes, commonly entered by humans
+		# Anything else we consider invalid
+		s = s.replace(' ', '').replace('-', '')
+	else:
 		return False
 
-	# check sum
-	ld = [ int(x) * 2 for n, x in enumerate(s[::-1]) if n % 2 != 0 ]
-	la = reduce(lambda x, y: x + y, map(lambda x:list(str(x)), ld))
-	lb = [x for n, x in enumerate(s[::-1]) if n % 2 == 0 ]
-	chksum = reduce(lambda x, y:int(x) + int(y), la + lb) % 10
-	return chksum == 0
+	return _cc.constraint(s) and isValidCreditCard(s)
+
+_digits_re = re.compile('^([0-9][0-9])')
 
 def validate_credit_card(number, exp_month, exp_year, cvc=None):
-	digits_re = re.compile('^([0-9][0-9])')
-
 	if not is_valid_creditcard_number(number):
-		raise schema.ValidationError('Invalid credit card number')
+		raise NotValidISO7812CreditCard(number)
 
-	if not digits_re.match(exp_month):
+	if not _digits_re.match(exp_month):
 		raise schema.ValidationError('Invalid expiration month')
 
-	if not digits_re.match(exp_year):
+	if not _digits_re.match(exp_year):
 		raise schema.ValidationError('Invalid expiration year')
 
 	if cvc and not all([c.isdigit() for c in str(cvc)]):
