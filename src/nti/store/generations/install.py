6412974547d38c5508b021b9c2b112ccfeecd4c9
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-01-08 23:37:23 +0000

    one day we will need a migration
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@29538 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/store/generations/install.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Store generation installation.
+
+$Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+generation = 1
+
+from zope.generations.generations import SchemaManager
+
+class _StoreSchemaManager(SchemaManager):
+	"""
+	A schema manager that we can register as a utility in ZCML.
+	"""
+	def __init__(self):
+		super(_StoreSchemaManager, self).__init__(
+											generation=generation,
+											minimum_generation=generation,
+											package_name='nti.store.generations')
+
+def evolve(context):
+	pass
