11ebbe2c8c6244e987b9e875922f3fefe282c53e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-02-21 01:14:40 +0000

    refactor a bit
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@33388 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/store/content_utils.py
+++ src/nti/store/content_utils.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-content utils
-
 $Id$
 """
 from __future__ import print_function, unicode_literals, absolute_import, division
@@ -14,19 +12,19 @@ from zope import component
 
 from nti.contentlibrary import interfaces as lib_interfaces
 
-def get_ntiid_path(ntiid, library=None, registry=component):
-    result = ()
+def get_paths(ntiid, library=None, registry=component):
     library = registry.queryUtility(lib_interfaces.IContentPackageLibrary) \
               if library is None else library
-    if library and ntiid:
-        paths = library.pathToNTIID(ntiid)
-        result = tuple([p.ntiid for p in paths]) if paths else ()
+    paths = library.pathToNTIID(ntiid) if library and ntiid else ()
+    return paths or ()
+
+def get_ntiid_path(ntiid, library=None, registry=component):
+    result = get_paths(ntiid, library, registry)
+    result = tuple([p.ntiid for p in result]) if result else ()
     return result
 
 def get_collection_root(ntiid, library=None, registry=component):
-    library = registry.queryUtility(lib_interfaces.IContentPackageLibrary) \
-              if library is None else library
-    paths = library.pathToNTIID(ntiid) if library else None
+    paths = get_paths(ntiid, library, registry)
     return paths[0] if paths else None
 
 def get_collection_root_ntiid(ntiid, library=None, registry=component):
