f897a3d5f8b1e20f347a9e351c57188d72cbae97
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-02-28 22:42:42 +0000

    add some comments
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@33955 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/store/payments/tests/test_utils.py
+++ src/nti/store/payments/tests/test_utils.py
@@ -4,8 +4,8 @@
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
-#disable: accessing protected members, too many methods
-#pylint: disable=W0212,R0904
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
 
 from hamcrest import is_
 from hamcrest import assert_that
