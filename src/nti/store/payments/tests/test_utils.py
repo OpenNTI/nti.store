72c778979556ede77b5b14e550274608c8194529
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-03-12 22:21:58 +0000

    use valid cc numbers for validation
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@34773 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/store/payments/tests/test_utils.py
+++ src/nti/store/payments/tests/test_utils.py
@@ -18,19 +18,22 @@ from nti.store.payments import utils
 
 from nose.tools import assert_raises
 
+from nti.store.tests import SharedConfiguringTestLayer
+
 class TestPaymentUtils(unittest.TestCase):
 
+	layer = SharedConfiguringTestLayer
+
 	def test_is_valid_creditcard_number(self):
-		assert_that(utils.is_valid_creditcard_number("4111-1111-1111-1111"), is_(True))
-		assert_that(utils.is_valid_creditcard_number("4111 1111 1111 1112"), is_(False))
-		assert_that(utils.is_valid_creditcard_number("5105105105105100"), is_(True))
-		assert_that(utils.is_valid_creditcard_number(5105105105105100), is_(True))
+		assert_that(utils.is_valid_creditcard_number("5418998592489835"), is_(True))
+		assert_that(utils.is_valid_creditcard_number("5504 0214 5972 7991"), is_(False))
+		assert_that(utils.is_valid_creditcard_number(4556919296095038), is_(True))
 		assert_that(utils.is_valid_creditcard_number("111"), is_(False))
 		assert_that(utils.is_valid_creditcard_number("5105105105105XY0"), is_(False))
 
 	def test_validate_credit_card(self):
-		with assert_raises(ValidationError):
-			utils.validate_credit_card(5105105105105100, "01", "12", "647")
+
+		utils.validate_credit_card(348728112862781, "01", "12", "647")
 
 		with assert_raises(ValidationError):
 			utils.validate_credit_card(5105105105105101, "01", "12", "647")
