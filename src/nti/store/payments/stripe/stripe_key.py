b5b7f3f0efde601a31ecae0bd53a60b2f2ace118
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-10-20 16:49:50 +0000

    add simple cypher
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.store/trunk@51532 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/store/payments/stripe/stripe_key.py
+++ src/nti/store/payments/stripe/stripe_key.py
@@ -8,6 +8,10 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import base64
+
+from Crypto.Cipher import XOR
+
 from zope import interface
 
 from zope.mimetype.interfaces import IContentTypeAware
@@ -24,13 +28,35 @@ from ...utils import MetaStoreObject
 
 from .interfaces import IStripeConnectKey
 
+DEFAULT_PASSPHRASE = base64.b64decode('TjN4dFRoMHVnaHQhIUM=')
+
+def make_ciphertext(plaintext, passphrase=DEFAULT_PASSPHRASE):
+	cipher = XOR.new(passphrase)
+	result = base64.b64encode(cipher.encrypt(plaintext))
+	return result
+	
+def get_plaintext(ciphertext, passphrase=DEFAULT_PASSPHRASE):
+	cipher = XOR.new(passphrase)
+	result = cipher.decrypt(base64.b64decode(ciphertext))
+	return result
+
 @interface.implementer(IStripeConnectKey, IContentTypeAware)
 @WithRepr
 @EqHash('Alias',)
 class StripeConnectKey(SchemaConfigured):
 	createDirectFieldProperties(IStripeConnectKey)
 
+	__metaclass__ = MetaStoreObject
+	
 	key = _('PrivateKey')
 	alias = name = _('Alias')
+
+	def __setattr__(self, name, value):
+		if name in ("key", "PrivateKey"):
+			try:
+				key = get_plaintext(value)
+				value = unicode(key)
+			except (TypeError, StandardError):
+				pass
+		return SchemaConfigured.__setattr__(self, name, value)
 	
-	__metaclass__ = MetaStoreObject
--- src/nti/store/payments/stripe/tests/test_zcml.py
+++ src/nti/store/payments/stripe/tests/test_zcml.py
@@ -12,6 +12,8 @@ from hamcrest import is_not
 from hamcrest import has_key
 from hamcrest import has_entry
 from hamcrest import assert_that
+from hamcrest import has_property
+does_not = is_not
 
 import unittest
 
@@ -19,7 +21,7 @@ from zope import component
 
 from nti.externalization.externalization import toExternalObject
 
-from nti.store.payments.stripe import interfaces as stripe_interfaces
+from nti.store.payments.stripe.interfaces import IStripeConnectKey
 
 from nti.store.tests import SharedConfiguringTestLayer
 
@@ -29,7 +31,9 @@ class TestZcml(unittest.TestCase):
 
 	def test_default_registrations(self):
 		cap_name = 'NTI-TEST'
-		sk = component.getUtility(stripe_interfaces.IStripeConnectKey, cap_name)
+		sk = component.getUtility(IStripeConnectKey, cap_name)
+		assert_that(sk, has_property('PrivateKey', 'sk_test_3K9VJFyfj0oGIMi7Aeg3HNBp'))
+				
 		assert_that(sk, is_not(none()))
 		extobj = toExternalObject(sk)
 		assert_that(extobj, has_key('MimeType'))
@@ -38,5 +42,5 @@ class TestZcml(unittest.TestCase):
 		assert_that(extobj, has_entry('LiveMode', False))
 		assert_that(extobj, has_entry('StripeUserID', 'NEXTTHOUGHT'))
 		assert_that(extobj, has_entry('PublicKey', 'pk_test_LIpQyLD7p5FmspOs6pPW9gWG'))
-
+		assert_that(extobj, does_not(has_key('PrivateKey')))
 
