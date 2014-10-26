1fdceb06e6996de0528b87c572bdcb17974f2426
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-10-26 00:52:43 +0000

    pass the registry
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.store/trunk@52141 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/store/payments/stripe/stripe_pricing.py
+++ src/nti/store/payments/stripe/stripe_pricing.py
@@ -15,7 +15,8 @@ from zope import component
 
 from . import NoSuchStripeCoupon
 from . import InvalidStripeCoupon
-from . import interfaces as stripe_interfaces
+
+from .interfaces import IStripeConnectKey
 
 from ... import PricingException
 
@@ -28,8 +29,8 @@ from .stripe_purchase import StripePricedPurchasable
 
 from . import STRIPE
 
-def get_coupon(coupon=None, api_key=None, processor=STRIPE):
-	manager = component.getUtility(IPaymentProcessor, name=processor)
+def get_coupon(coupon=None, api_key=None, processor=STRIPE, registry=component):
+	manager = registry.getUtility(IPaymentProcessor, name=processor)
 	if coupon is not None and api_key:
 		# stripe defines an 80 sec timeout for http requests
 		# at this moment we are to wait for coupon validation
@@ -50,23 +51,27 @@ class StripePurchasablePricer(DefaultPurchasablePricer):
 
 	processor = STRIPE
 
-	def get_coupon(self, coupon=None, api_key=None):
-		result = get_coupon(coupon=coupon, api_key=api_key, processor=self.processor)
+	def get_coupon(self, coupon=None, api_key=None, registry=component):
+		result = get_coupon(coupon=coupon, 
+							api_key=api_key, 
+							processor=self.processor,
+							registry=component)
 		return result
 
-	def price(self, priceable):
+	def price(self, priceable, registry=component):
 		priced = super(StripePurchasablePricer, self).price(priceable)
 		priced = StripePricedPurchasable.copy(priced)
 		coupon = getattr(priceable, 'Coupon', None)
 
-		manager = component.getUtility(IPaymentProcessor, name=self.processor)
-		stripe_key = component.queryUtility(stripe_interfaces.IStripeConnectKey,
-										    priced.Provider)
+		manager = registry.getUtility(IPaymentProcessor, name=self.processor)
+		stripe_key = registry.queryUtility(IStripeConnectKey, priced.Provider)
 
 		purchase_price = priced.PurchasePrice
 
 		if coupon is not None and stripe_key:
-			priced.Coupon = self.get_coupon(coupon, stripe_key.PrivateKey)
+			priced.Coupon = self.get_coupon(coupon=coupon, 
+											stripe_key.PrivateKey, 
+											registry=registry)
 			if priced.Coupon is not None:
 				priced.NonDiscountedPrice = purchase_price
 				purchase_price = manager.apply_coupon(purchase_price, priced.Coupon)
@@ -74,14 +79,14 @@ class StripePurchasablePricer(DefaultPurchasablePricer):
 				priced.PurchaseFee = self.calc_fee(purchase_price, priceable.Fee)
 		return priced
 
-	def evaluate(self, priceables):
+	def evaluate(self, priceables, registry=component):
 		providers = set()
 		currencies = set()
 		result = create_pricing_results(non_discounted_price=0.0)
 		for priceable in priceables:
 			currencies.add(priceable.Currency)
 			providers.add(priceable.Provider)
-			priced = self.price(priceable)
+			priced = self.price(priceable, registry)
 			result.Items.append(priced)
 			result.TotalPurchaseFee += priced.PurchaseFee
 			result.TotalPurchasePrice += priced.PurchasePrice
@@ -99,11 +104,9 @@ class StripePurchasablePricer(DefaultPurchasablePricer):
 				raise PricingException("Multi-Provider coupon purchase is not supported")
 
 			provider = providers.pop()
-			stripe_key = component.queryUtility(stripe_interfaces.IStripeConnectKey,
-												provider)
-			coupon = self.get_coupon(coupon, stripe_key.PrivateKey) \
-					 if stripe_key else None
-			manager = component.getUtility(IPaymentProcessor, name=self.processor)
+			stripe_key = registry.queryUtility(IStripeConnectKey, provider)
+			coupon = self.get_coupon(coupon, stripe_key.PrivateKey) if stripe_key else None
+			manager = registry.getUtility(IPaymentProcessor, name=self.processor)
 			if coupon is not None:
 				result.NonDiscountedPrice = result.TotalPurchasePrice
 				purchase_price = manager.apply_coupon(result.TotalPurchasePrice, coupon)
