8af40bf82aa6434defe994a6a667d01693f70dba
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-01-26 14:11:04 +0000

    capture type error
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.store/trunk@58291 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/store/metadata_index.py
+++ src/nti/store/metadata_index.py
@@ -21,6 +21,8 @@ from nti.dataserver.interfaces import IUser
 from nti.dataserver.interfaces import ISystemUserPrincipal
 from nti.dataserver.interfaces import IPrincipalMetadataObjectsIntIds
 
+from nti.utils.property import Lazy
+
 from .store import get_gift_registry
 from .store import get_user_purchase_history
 
@@ -35,39 +37,39 @@ def get_uid(obj, intids=None):
 				logger.warn("ignoring unregistered object %s", obj)
 			else:
 				return uid
-	except (POSError):
+	except (TypeError, POSError):
 		logger.error("ignoring broken object %s", type(obj))
 	return None
 
-@component.adapter(IUser)
 @interface.implementer(IPrincipalMetadataObjectsIntIds)
-class _PurchaseAttemptPrincipalObjectsIntIds(object):
+class _BasePrincipalObjectsIntIds(object):
 
-	__slots__ = ('user',)
+	@Lazy
+	def _intids(self):
+		return component.getUtility(zope.intid.IIntIds)
+
+@component.adapter(IUser)
+class _PurchaseAttemptPrincipalObjectsIntIds(_BasePrincipalObjectsIntIds):
 
 	def __init__(self, user):
 		self.user = user
 
 	def iter_intids(self):
 		user = self.user
-		intids = component.getUtility(zope.intid.IIntIds) 
 		for purchase in get_user_purchase_history(user):
-			uid = get_uid(purchase, intids=intids)
+			uid = get_uid(purchase, intids=self._intids)
 			if uid is not None:
 				yield uid
 
 @component.adapter(ISystemUserPrincipal)
-@interface.implementer(IPrincipalMetadataObjectsIntIds)
-class _GiftPurchaseAttemptPrincipalObjectsIntIds(object):
+class _GiftPurchaseAttemptPrincipalObjectsIntIds(_BasePrincipalObjectsIntIds):
 
-	__slots__ = ()
-	
 	def __init__(self, *args, **kwargs):
 		pass
 
 	def iter_intids(self, intids=None):
 		registry = get_gift_registry()
-		intids = component.getUtility(zope.intid.IIntIds) if intids is None else intids
+		intids = self._intids if intids is None else intids
 		for username in registry.keys():
 			for gift in registry.get_purchase_history(username):
 				uid = get_uid(gift, intids=intids)
