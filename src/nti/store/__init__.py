d55cd27966059272709c6e46da83dd7dfdda8d7d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-04-13 19:21:11 +0000

    add helper function
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.store/trunk@63030 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/store/__init__.py
+++ src/nti/store/__init__.py
@@ -3,6 +3,7 @@
 """
 .. $Id$
 """
+
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
@@ -11,7 +12,10 @@ logger = __import__('logging').getLogger(__name__)
 import zope.i18nmessageid
 MessageFactory = zope.i18nmessageid.MessageFactory('nti.dataserver')
 
+from zope import component
 from zope import interface
+
+from zope.catalog.interfaces import ICatalog
 
 from nti.dataserver.users import User
 from nti.dataserver.interfaces import IUser
@@ -28,6 +32,8 @@ PURCHASABLE_COURSE = 'purchasable_course'
 PURCHASABLE_CONTENT = 'purchasable_content'
 PURCHASABLE_CHOICE_BUNDLE = 'purchasable_choice_bundle'
 PURCHASABLE_COURSE_CHOICE_BUNDLE = 'purchasable_course_choice_bundle'
+
+CATALOG_NAME = 'nti.dataserver.++etc++purchase-catalog'
 
 @interface.implementer(INTIStoreException)
 class NTIStoreException(Exception):
@@ -55,3 +61,7 @@ class RedemptionException(NTIStoreException):
 def get_user(user):
 	result = User.get_user(str(user)) if user and not IUser.providedBy(user) else user
 	return result
+
+def get_purchase_index():
+	return component.getUtility(ICatalog, name=CATALOG_NAME)
+get_index = get_purchase_index
