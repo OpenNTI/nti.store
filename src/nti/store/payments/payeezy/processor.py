807bd0509304d2d03ece7b119ab6b8d60c14e63d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-04-26 19:30:01 +0000

    add configure zcml and stub out processor
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.store/trunk@111585 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/store/payments/payeezy/processor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import interface
+
+from nti.store.payments.payeezy.interfaces import IPayeezyPaymentProcessor
+
+
+@interface.implementer(IPayeezyPaymentProcessor)
+class StripePaymentProcessor(object):
+    pass
