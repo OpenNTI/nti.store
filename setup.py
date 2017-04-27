bb055d4712bcdf4998c29a1f6ecb2e477cf521ad
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-04-27 19:46:31 +0000

    add back payeezy
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.store/trunk@111683 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -4,35 +4,36 @@ from setuptools import setup, find_packages
 VERSION = '0.0.0'
 
 entry_points = {
-	'console_scripts': [
-	],
+    'console_scripts': [
+    ],
 }
 
 setup(
-	name='nti.store',
-	version=VERSION,
-	author='Jason Madden',
-	author_email='jason@nextthought.com',
-	description="NTI Store",
-	long_description=codecs.open('README.rst', encoding='utf-8').read(),
-	license='Proprietary',
-	keywords='Store purchase',
-	classifiers=[
-		'Intended Audience :: Developers',
-		'Natural Language :: English',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: Implementation :: CPython'
-	],
-	packages=find_packages('src'),
-	package_dir={'': 'src'},
-	namespace_packages=['nti'],
-	install_requires=[
-		'setuptools',
-		'requests',
-		'stripe',
-		'nti.contentlibrary'
-	],
-	entry_points=entry_points
+    name='nti.store',
+    version=VERSION,
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="NTI Store",
+    long_description=codecs.open('README.rst', encoding='utf-8').read(),
+    license='Proprietary',
+    keywords='Store purchase',
+    classifiers=[
+                'Intended Audience :: Developers',
+                'Natural Language :: English',
+                'Operating System :: OS Independent',
+                'Programming Language :: Python :: 2',
+                'Programming Language :: Python :: 2.7',
+                'Programming Language :: Python :: Implementation :: CPython'
+    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['nti'],
+    install_requires=[
+        'setuptools',
+        'payeezy',
+        'requests',
+        'stripe',
+        'nti.contentlibrary'
+    ],
+    entry_points=entry_points
 )
