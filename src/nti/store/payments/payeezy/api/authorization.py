095daaa317831642cdc97820c10684c118ba76dc
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-04-27 20:10:28 +0000

    add some API methods
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.store/trunk@111685 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/store/payments/payeezy/api/authorization.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+import os
+import ssl
+import hmac
+import time
+import hashlib
+from base64 import b64encode
+
+import simplejson as json
+
+import requests
+
+from requests.adapters import HTTPAdapter
+
+from requests.packages.urllib3.poolmanager import PoolManager
+
+
+class TLSv1Adapter(HTTPAdapter):
+    def init_poolmanager(self, connections, maxsize, block=False):
+        self.poolmanager = PoolManager(num_pools=connections,
+                                       maxsize=maxsize,
+                                       block=block,
+                                       ssl_version=ssl.PROTOCOL_TLSv1)
+
+
+class PayeezyHTTPAuthorize(object):
+
+    def __init__(self, api_key, api_secret, token, url, token_url):
+        self.token = token
+        self.api_key = api_key
+        self.api_secret = api_secret
+        # urls
+        self.url = url
+        self.token_url = token_url
+        # cryptographically strong random number
+        self.nonce = str(int(os.urandom(16).encode('hex'), 16))
+        self.timestamp = str(int(round(time.time() * 1000)))
+        # timeout
+        self.timeout = 30
+
+    # HMAC Generation
+    def generate_hmac_authentication_header(self, payload):
+        message_data = self.api_key + self.nonce + self.timestamp + self.token + payload
+        hmacInHex = hmac.new(self.api_secret, msg=message_data,
+                             digestmod=hashlib.sha256).hexdigest()
+        return b64encode(hmacInHex)
+
+    # method to make calls for getToken
+    def get_token_post_call(self, payload):
+        response = requests.Session()
+        response.mount('https://', TLSv1Adapter())
+        self.payload = json.dumps(payload)
+        authorization = self.generate_hmac_authentication_header(self.payload)
+        result = response.post(self.token_url,
+                               headers={'User-Agent': 'Payeezy-Python',
+                                        'content-type': 'application/json',
+                                        'api_key': self.api_key,
+                                        'token': self.token,
+                                        'Authorization': authorization},
+                               data=self.payload)
+        return result
+
+    # Generic method to make calls for primary transactions
+    def make_card_based_transaction_post_call(self, payload):
+        response = requests.Session()
+        response.mount('https://', TLSv1Adapter())
+        self.payload = json.dumps(payload)
+        authorization = self.generate_hmac_authentication_header(self.payload)
+        result = response.post(self.url,
+                               headers={'User-Agent': 'Payeezy-Python',
+                                        'content-type': 'application/json',
+                                        'api_key': self.api_key,
+                                        'token': self.token,
+                                        'nonce': self.nonce,
+                                        'timestamp': self.timestamp,
+                                        'Authorization': authorization},
+                               data=self.payload)
+        return result
+
+    # Generic method to make calls for secondary transactions
+    def make_capture_void_refund_post_call(self, payload, transactionID):
+        response = requests.Session()
+        response.mount('https://', TLSv1Adapter())
+        self.url = self.url + '/' + transactionID
+        self.payload = json.dumps(payload)
+        authorization = self.generate_hmac_authentication_header(self.payload)
+        result = response.post(self.url,
+                               headers={'User-Agent': 'Payeezy-Python',
+                                        'content-type': 'application/json',
+                                        'api_key': self.api_key,
+                                        'token': self.token,
+                                        'nonce': self.nonce,
+                                        'timestamp': self.timestamp,
+                                        'Authorization': authorization},
+                               data=self.payload)
+        return result
---
+++ src/nti/store/payments/payeezy/api/methods.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+import six
+
+from nti.store.payments.payeezy.api import authorization
+
+
+class Payeezy(object):
+
+    def __init__(self, api_key, api_secret, token, url, token_url):
+        self.token = token
+        self.api_key = api_key
+        self.api_secret = api_secret
+        # urls
+        self.url = url
+        self.token_url = token_url
+
+    def authorize(self, amount=None, currency_code=None, description=None,
+                  card_type=None, cardholder_name=None, card_number=None,
+                  card_expiry=None, card_cvv=None):
+
+        make_payload_output = self.make_payload(amount=amount,
+                                                currency_code=currency_code,
+                                                card_type=card_type,
+                                                cardholder_name=cardholder_name,
+                                                card_number=card_number,
+                                                card_expiry=card_expiry,
+                                                card_cvv=card_cvv,
+                                                description=description,
+                                                transaction_type='authorize')
+
+        return self.make_primary_transaction(payload=make_payload_output['payload'])
+
+    def purchase(self, amount=None, currency_code=None, description=None,
+                 card_type=None, cardholder_name=None, card_number=None,
+                 card_expiry=None, card_cvv=None, ):
+
+        make_payload_output = self.make_payload(amount=amount,
+                                                currency_code=currency_code,
+                                                card_type=card_type,
+                                                cardholder_name=cardholder_name,
+                                                card_number=card_number,
+                                                card_expiry=card_expiry,
+                                                card_cvv=card_cvv,
+                                                description=description,
+                                                transaction_type='purchase')
+
+        return self.make_primary_transaction(payload=make_payload_output['payload'])
+
+    def capture(self, amount=None, currency_code=None, description=None,
+                transaction_tag=None, transactionID=None):
+
+        make_payload_output = self.make_payload(amount=amount,
+                                                currency_code=currency_code,
+                                                transaction_tag=transaction_tag,
+                                                transactionID=transactionID,
+                                                description=description,
+                                                transaction_type='capture')
+
+        return self.make_secondary_transaction(payload=make_payload_output['payload'],
+                                               transactionID=make_payload_output['transactionID'])
+
+    def void(self,  payload):
+        self.payload = payload
+        self.transaction_type = "void"
+        return self.make_secondary_transaction(self.transaction_type, self.payload)
+
+    def refund(self,  payload):
+        self.payload = payload
+        self.transaction_type = "refund"
+        return self.make_secondary_transaction(self.transaction_type, self.payload)
+
+    def make_primary_transaction(self, payload):
+        self.payload = payload
+        self.payeezy = authorization.PayeezyHTTPAuthorize(self.api_key, 
+                                                          self.api_secret,
+                                                          self.token, 
+                                                          self.url, 
+                                                          self.tokenurl)
+        return self.payeezy.makeCardBasedTransactionPostCall(self.payload)
+
+    def make_secondary_transaction(self, payload, transactionID):
+        self.payload = payload
+        self.transactionID = transactionID
+        self.payeezy = authorization.PayeezyHTTPAuthorize(self.api_key, 
+                                                          self.api_secret, 
+                                                          self.token, 
+                                                          self.url, 
+                                                          self.tokenurl)
+        return self.payeezy.makeCaptureVoidRefundPostCall(self.payload, self.transactionID)
+
+    def make_payload(self, amount=None, currency_code='USD',  description=None,
+                     card_type=None, cardholder_name=None, card_number=None,
+                     card_expiry=None, card_cvv=None, transaction_type=None,
+                     transaction_tag=None, transactionID=None):
+
+        assert amount is not None, "Amount cannot be None"
+        if isinstance(object, six.integer_types):
+            amount = str(amount)
+
+        assert currency_code, "Currency cannot be None"
+        assert transaction_type, "Transaction Type cannot be None"
+
+        # fill some description
+        if description is None:
+            description = transaction_type + 'transaction for amount: ' + amount
+
+        if transaction_type in ('authorize', 'purchase'):
+            assert card_number is not None, "Card number cannot be None"
+            if isinstance(card_number, six.integer_types):
+                card_number = str(card_number)
+
+            assert cardholder_name, "Card name was not provided"
+
+            assert card_cvv is not None, "CVV number cannot be None"
+            if isinstance(card_cvv, six.integer_types):
+                card_cvv = str(card_cvv)
+
+            assert card_expiry is not None, "Expiry cannot be None"
+            if isinstance(card_expiry, six.integer_types):
+                card_expiry = str(card_expiry)
+
+            payload = {
+                "merchant_ref": description,
+                "transaction_type": transaction_type,
+                "method": "credit_card",
+                "amount": amount,
+                "currency_code": currency_code.upper(),
+                "credit_card": {
+                    "type": card_type,
+                    "cardholder_name": cardholder_name,
+                    "card_number": card_number,
+                    "exp_date": card_expiry,
+                    "cvv": card_cvv
+                }
+            }
+        else:
+            assert transactionID, "Transaction ID cannot be None"
+            assert transaction_tag, "Transaction Tag cannot be None"
+            if isinstance(transaction_tag, six.integer_types):
+                transaction_tag = str(transaction_tag)
+
+            payload = {
+                "merchant_ref": description,
+                "transaction_tag": transaction_tag,
+                "transaction_type": transaction_type,
+                "method": "credit_card", "amount": amount,
+                "currency_code": currency_code.upper()
+            }
+
+        return {'payload': payload, 'transactionID': transactionID}
