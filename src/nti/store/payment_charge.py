0f5b7fdb69f1eb06faccf8ce0779ca5c7240fa37
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-04-28 14:18:50 +0000

    remove extra def; pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.store/trunk@111744 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/store/payment_charge.py
+++ src/nti/store/payment_charge.py
@@ -30,38 +30,40 @@ from nti.store.interfaces import IPaymentCharge
 
 from nti.store.utils import MetaStoreObject
 
+
 @WithRepr
 @EqHash('Zip', 'City', 'State', 'Street', 'Country')
 @interface.implementer(IUserAddress, IContentTypeAware)
 class UserAddress(SchemaConfigured):
 
-	__metaclass__ = MetaStoreObject
+    __metaclass__ = MetaStoreObject
 
-	Street = FP(IUserAddress['Street'])
-	City = FP(IUserAddress['City'])
-	State = FP(IUserAddress['State'])
-	Zip = FP(IUserAddress['Zip'])
-	Country = FP(IUserAddress['Country'])
+    Street = FP(IUserAddress['Street'])
+    City = FP(IUserAddress['City'])
+    State = FP(IUserAddress['State'])
+    Zip = FP(IUserAddress['Zip'])
+    Country = FP(IUserAddress['Country'])
 
-	parameters = {}  # IContentTypeAware
+    parameters = {}  # IContentTypeAware
 
-	def __str__(self):
-		return "%s\n%s,%s %s\n%s" % (self.Street,
-									 self.City,
-									 self.State,
-									 self.Zip,
-									 self.Country)
+    def __str__(self):
+        return "%s\n%s,%s %s\n%s" % (self.Street,
+                                     self.City,
+                                     self.State,
+                                     self.Zip,
+                                     self.Country)
 
-	@classmethod
-	def create(cls, address_line1, address_line2=None, city=None, state=None,
-			   zip_=None, country=None):
-		city = city or u''
-		zip_ = zip_ or IUserAddress['Zip'].default
-		country = country or IUserAddress['Country'].default
-		street = "%s\n%s" % (address_line1, address_line2 or u'')
-		result = UserAddress(Street=street.strip(), City=city, State=state,
-							 Zip=zip_, Country=country)
-		return result
+    @classmethod
+    def create(cls, address_line1, address_line2=None, city=None, state=None,
+               zip_=None, country=None):
+        city = city or u''
+        zip_ = zip_ or IUserAddress['Zip'].default
+        country = country or IUserAddress['Country'].default
+        street = "%s\n%s" % (address_line1, address_line2 or u'')
+        result = UserAddress(Street=street.strip(), City=city, State=state,
+                             Zip=zip_, Country=country)
+        return result
+
 
 @WithRepr
 @total_ordering
@@ -69,28 +71,26 @@ class UserAddress(SchemaConfigured):
 @interface.implementer(IPaymentCharge, IContentTypeAware)
 class PaymentCharge(SchemaConfigured):
 
-	__metaclass__ = MetaStoreObject
+    __metaclass__ = MetaStoreObject
 
-	Name = FP(IPaymentCharge['Name'])
-	Amount = FP(IPaymentCharge['Amount'])
-	Currency = FP(IPaymentCharge['Currency'])
-	Created = FP(IPaymentCharge['Created'])
-	CardLast4 = FP(IPaymentCharge['CardLast4'])
-	Address = FP(IPaymentCharge['Address'])
+    Name = FP(IPaymentCharge['Name'])
+    Amount = FP(IPaymentCharge['Amount'])
+    Currency = FP(IPaymentCharge['Currency'])
+    Created = FP(IPaymentCharge['Created'])
+    CardLast4 = FP(IPaymentCharge['CardLast4'])
+    Address = FP(IPaymentCharge['Address'])
 
-	parameters = {}  # IContentTypeAware
+    def __str__(self):
+        return "%s:%s" % (self.Currency, self.Amount)
 
-	def __str__(self):
-		return "%s:%s" % (self.Currency, self.Amount)
+    def __lt__(self, other):
+        try:
+            return self.Created < other.Created
+        except AttributeError:
+            return NotImplemented
 
-	def __lt__(self, other):
-		try:
-			return self.Created < other.Created
-		except AttributeError:
-			return NotImplemented
-
-	def __gt__(self, other):
-		try:
-			return self.Created > other.Created
-		except AttributeError:
-			return NotImplemented
+    def __gt__(self, other):
+        try:
+            return self.Created > other.Created
+        except AttributeError:
+            return NotImplemented
