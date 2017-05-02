63a6b6aa1670edfc5fe2006e49431aef8129ef47
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-02 21:14:18 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.store/trunk@111933 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/store/tests/test_course.py
+++ src/nti/store/tests/test_course.py
@@ -17,7 +17,6 @@ from hamcrest import has_entries
 does_not = is_not
 
 import unittest
-import dateutil.parser
 
 from nti.externalization.externalization import to_external_object
 
@@ -25,57 +24,58 @@ from nti.store.course import create_course
 
 from nti.store.tests import SharedConfiguringTestLayer
 
+
 class TestPurchasableCourse(unittest.TestCase):
 
-	layer = SharedConfiguringTestLayer
+    layer = SharedConfiguringTestLayer
 
-	processor = 'stripe'
+    processor = 'stripe'
 
-	def test_create_course(self):
-		default_date_str = "2015-06-13T04:59:00+00:00"
+    def test_create_course(self):
+        default_date_str = "2015-06-13T04:59:00+00:00"
 
-		course = create_course(
-					ntiid='tag:nextthought.com,2011-10:NTI-purchasable_course-LSTD_1153',
-					name="A History of the United States",
-					provider="Janux",
-					amount=500,
-					items=("tag:nextthought.com,2011-10:NTI-CourseInfo-Spring2015_LSTD_1153",),
-					title="A History of the United States",
-					author="Janux",
-					giftable=True,
-					description="History is about more than the people",
-					vendor_info={"CRN" : 34846, "Term": 201410},
-					purchase_cutoff_date=default_date_str,
-					redeem_cutoff_date=default_date_str)
-		ext = to_external_object(course)
-		assert_that(ext,
-					has_entries('Amount', 500.0,
-								'Class','PurchasableCourse',
-								'Currency', u'USD',
-								'Description', u'History is about more than the people',
-								'Giftable', True,
-								'NTIID', u'tag:nextthought.com,2011-10:NTI-purchasable_course-LSTD_1153',
-								'MimeType', 'application/vnd.nextthought.store.purchasablecourse',
-								'Public', True,
-								'Redeemable', False,
-								'Title', u'A History of the United States',
-								'PurchaseCutOffDate', default_date_str,
-								'RedeemCutOffDate', default_date_str,
-								'VendorInfo', is_({u'CRN': 34846, u'Term': 201410})))
+        course = create_course(
+            ntiid='tag:nextthought.com,2011-10:NTI-purchasable_course-LSTD_1153',
+            name="A History of the United States",
+            provider="Janux",
+            amount=500,
+            items=("tag:nextthought.com,2011-10:NTI-CourseInfo-Spring2015_LSTD_1153",),
+            title="A History of the United States",
+            author="Janux",
+            giftable=True,
+            description="History is about more than the people",
+            vendor_info={"CRN": 34846, "Term": 201410},
+            purchase_cutoff_date=default_date_str,
+            redeem_cutoff_date=default_date_str)
+        ext = to_external_object(course)
+        assert_that(ext,
+                    has_entries('Amount', 500.0,
+                                'Class', 'PurchasableCourse',
+                                'Currency', u'USD',
+                                'Description', u'History is about more than the people',
+                                'Giftable', True,
+                                'NTIID', u'tag:nextthought.com,2011-10:NTI-purchasable_course-LSTD_1153',
+                                'MimeType', 'application/vnd.nextthought.store.purchasablecourse',
+                                'Public', True,
+                                'Redeemable', False,
+                                'Title', u'A History of the United States',
+                                'PurchaseCutOffDate', default_date_str,
+                                'RedeemCutOffDate', default_date_str,
+                                'VendorInfo', is_({u'CRN': 34846, u'Term': 201410})))
 
-		ext = to_external_object(course, name="summary")
-		assert_that(ext, does_not(has_key('Icon')))
-		assert_that(ext, does_not(has_key('Public')))
-		assert_that(ext, does_not(has_key('License')))
-		assert_that(ext, does_not(has_key('Thumbnail')))
-		assert_that(ext, does_not(has_key('Description')))
-		assert_that(ext, does_not(has_key('EndDate')))
-		assert_that(ext, does_not(has_key('Preview')))
-		assert_that(ext, does_not(has_key('Featured')))
-		assert_that(ext, does_not(has_key('Duration')))
-		assert_that(ext, does_not(has_key('StartDate')))
-		assert_that(ext, does_not(has_key('Signature')))
-		assert_that(ext, does_not(has_key('Department')))
-		assert_that(ext, does_not(has_key('Communities')))
+        ext = to_external_object(course, name="summary")
+        assert_that(ext, does_not(has_key('Icon')))
+        assert_that(ext, does_not(has_key('Public')))
+        assert_that(ext, does_not(has_key('License')))
+        assert_that(ext, does_not(has_key('Thumbnail')))
+        assert_that(ext, does_not(has_key('Description')))
+        assert_that(ext, does_not(has_key('EndDate')))
+        assert_that(ext, does_not(has_key('Preview')))
+        assert_that(ext, does_not(has_key('Featured')))
+        assert_that(ext, does_not(has_key('Duration')))
+        assert_that(ext, does_not(has_key('StartDate')))
+        assert_that(ext, does_not(has_key('Signature')))
+        assert_that(ext, does_not(has_key('Department')))
+        assert_that(ext, does_not(has_key('Communities')))
 
-		assert_that(ext, has_entry('VendorInfo', has_length(2)))
+        assert_that(ext, has_entry('VendorInfo', has_length(2)))
