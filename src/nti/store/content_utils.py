63444d275e246b458bf31f302c3aeba10f1db542
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-08 22:59:37 +0000

    remove unsued param
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.store/trunk@112277 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/store/content_utils.py
+++ src/nti/store/content_utils.py
@@ -4,7 +4,7 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -14,30 +14,30 @@ from zope import component
 from nti.contentlibrary.interfaces import IContentPackageLibrary
 
 
-def get_library(library=None, registry=component):
+def get_library(library=None):
     if library is None:
-        return registry.queryUtility(IContentPackageLibrary)
+        return component.queryUtility(IContentPackageLibrary)
     return library
 
 
-def get_paths(ntiid, library=None, registry=component):
-    library = get_library(registry=registry)
+def get_paths(ntiid, library=None):
+    library = get_library()
     paths = library.pathToNTIID(ntiid) if library and ntiid else ()
     return paths or ()
 
 
-def get_ntiid_path(ntiid, library=None, registry=component):
-    result = get_paths(ntiid, library, registry)
+def get_ntiid_path(ntiid, library=None):
+    result = get_paths(ntiid, library)
     result = tuple(p.ntiid for p in result) if result else ()
     return result
 
 
-def get_collection_root(ntiid, library=None, registry=component):
-    paths = get_paths(ntiid, library, registry)
+def get_collection_root(ntiid, library=None):
+    paths = get_paths(ntiid, library)
     return paths[0] if paths else None
 
 
-def get_collection_root_ntiid(ntiid, library=None, registry=component):
-    root = get_collection_root(ntiid, library, registry)
+def get_collection_root_ntiid(ntiid, library=None):
+    root = get_collection_root(ntiid, library)
     result = root.ntiid if root is not None else None
     return result
