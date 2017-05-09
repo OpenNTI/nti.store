1fcbd5692cf4ba670435eb7fa7714caf84cba7ad
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-09 18:56:25 +0000

    remove registry param
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.store/trunk@112374 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/store/payments/stripe/processor/pricing.py
+++ src/nti/store/payments/stripe/processor/pricing.py
@@ -4,7 +4,7 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -20,27 +20,25 @@ from nti.store.payments.stripe.processor.base import BaseProcessor
 from nti.store.purchase_history import get_purchase_attempt
 
 
-def price_order(order, name=STRIPE, registry=component):
-    pricer = registry.getUtility(IPurchasablePricer, name=name)
-    result = pricer.evaluate(order, registry=registry)
+def price_order(order, name=STRIPE):
+    pricer = component.getUtility(IPurchasablePricer, name=name)
+    result = pricer.evaluate(order)
     return result
 
 
-def price_purchase(purchase_attempt, name=STRIPE, registry=component):
-    result = price_order(purchase_attempt.Order, registry=registry)
+def price_purchase(purchase_attempt, name=STRIPE):
+    result = price_order(purchase_attempt.Order)
     return result
 
 
 class PricingProcessor(BaseProcessor):
 
-    def do_pricing(self, purchase_attempt, registry=component):
-        result = price_purchase(purchase_attempt,
-                                name=self.name,
-                                registry=registry)
+    def do_pricing(self, purchase_attempt):
+        result = price_purchase(purchase_attempt, name=self.name,)
         return result
     _do_pricing = do_pricing  # BWC
 
-    def price_purchase(self, purchase_id, username, registry=component):
+    def price_purchase(self, purchase_id, username=None):
         purchase = get_purchase_attempt(purchase_id, username)
-        result = self.do_pricing(purchase, registry=registry)
+        result = self.do_pricing(purchase)
         return result
