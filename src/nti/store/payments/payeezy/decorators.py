045dd7aaab9894d304d65d436c0fe89bc85962f3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-11 19:16:40 +0000

    add decorator
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.store/trunk@112508 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/store/payments/payeezy/decorators.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+from zope import interface
+
+from nti.externalization.singleton import SingletonDecorator
+from nti.externalization.interfaces import IExternalObjectDecorator
+
+from nti.store.interfaces import IPurchaseAttempt
+
+from nti.store.payments.payeezy import PAYEEZY
+
+from nti.store.payments.payeezy.interfaces import IPayeezyPurchaseAttempt
+
+
+@component.adapter(IPurchaseAttempt)
+@interface.implementer(IExternalObjectDecorator)
+class PurchaseAttemptDecorator(object):
+
+    __metaclass__ = SingletonDecorator
+
+    def decorateExternalObject(self, original, external):
+        if original.Processor == PAYEEZY:
+            ps = IPayeezyPurchaseAttempt(original)
+            external['TokenID'] = ps.token
+            external['TokeType'] = ps.token_type
+            external['TransactionId'] = ps.transaction_id
