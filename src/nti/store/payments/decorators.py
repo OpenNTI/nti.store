646c8571a56e3410113046280bc624002be75916
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-12 21:29:46 +0000

    add place holder decorator
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.store/trunk@112627 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/store/payments/decorators.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+from zope import interface
+
+from nti.externalization.singleton import SingletonDecorator
+from nti.externalization.interfaces import IExternalObjectDecorator
+
+from nti.store.interfaces import IPurchasable
+
+
+@component.adapter(IPurchasable)
+@interface.implementer(IExternalObjectDecorator)
+class PurchasableDecorator(object):
+
+    __metaclass__ = SingletonDecorator
+
+    def decorateExternalObject(self, original, external):
+        pass
+
