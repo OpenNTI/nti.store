9c4348c30864b1b7fa129d9d6782c2dea30cdbb0
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-12 02:36:47 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.store/trunk@112541 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/store/utils.py
+++ src/nti/store/utils.py
@@ -24,23 +24,25 @@ from nti.store.interfaces import ICopier
 STORE_MIME_BASE = MIME_BASE + b'.store'
 
 #: Invitation purchase attempt
-INVITATION_PURCHASE_ATTEMPT_MIME_TYPE = STORE_MIME_BASE + b'.invitationpurchaseattempt'
+INVITATION_PURCHASE_ATTEMPT_MIME_TYPE = \
+    STORE_MIME_BASE + b'.invitationpurchaseattempt'
 
 #: Redeem purchase attempt
 REDEEM_PURCHASE_ATTEMPT_MIME_TYPE = STORE_MIME_BASE + b'.redeemedpurchaseattempt'
 
 #: Non-Gift purchase attempt MimeTypes
 NONGIFT_PURCHASE_ATTEMPT_MIME_TYPES = \
-		tuple([STORE_MIME_BASE + x for x in (b'.purchaseattempt',
-											 b'.invitationpurchaseattempt',
-											 b'.redeemedpurchaseattempt') ])
+    tuple([STORE_MIME_BASE + x for x in (b'.purchaseattempt',
+                                         b'.invitationpurchaseattempt',
+                                         b'.redeemedpurchaseattempt')])
 
 #: Gift purchase attempt MimeTypes
-GIFT_PURCHASE_ATTEMPT_MIME_TYPES = tuple([STORE_MIME_BASE + b'.giftpurchaseattempt'])
+GIFT_PURCHASE_ATTEMPT_MIME_TYPES = \
+    tuple([STORE_MIME_BASE + b'.giftpurchaseattempt'])
 
 #: Purchase attempt MimeTypes
 PURCHASE_ATTEMPT_MIME_TYPES = \
-		NONGIFT_PURCHASE_ATTEMPT_MIME_TYPES + GIFT_PURCHASE_ATTEMPT_MIME_TYPES
+    NONGIFT_PURCHASE_ATTEMPT_MIME_TYPES + GIFT_PURCHASE_ATTEMPT_MIME_TYPES
 
 #: Purchasables MimeTypes
 PURCHASABLE_MIME_TYPES = []
@@ -55,63 +57,73 @@ ALL_STORE_MIME_TYPES = list(PURCHASE_ATTEMPT_MIME_TYPES)
 ALL_STORE_MIME_TYPES.extend(PURCHASABLE_MIME_TYPES)
 ALL_STORE_MIME_TYPES = tuple(ALL_STORE_MIME_TYPES)
 
+
 def get_ntiid_type(mimeType):
-	if mimeType == PURCHASABLE_MIME_TYPES[0]:  # purchasable
-		return PURCHASABLE
-	elif mimeType == PURCHASABLE_MIME_TYPES[1]:  # purchasable course
-		return PURCHASABLE_COURSE
-	elif mimeType == PURCHASABLE_CHOICE_BUNDLE[2]:  # purchasable choice bundle
-		return PURCHASABLE_CHOICE_BUNDLE
-	elif mimeType == PURCHASABLE_COURSE_CHOICE_BUNDLE[3]:  # purchasable course choice bundle
-		return PURCHASABLE_COURSE_CHOICE_BUNDLE
-	return None
+    if mimeType == PURCHASABLE_MIME_TYPES[0]:  # purchasable
+        return PURCHASABLE
+    elif mimeType == PURCHASABLE_MIME_TYPES[1]:  # purchasable course
+        return PURCHASABLE_COURSE
+    elif mimeType == PURCHASABLE_CHOICE_BUNDLE[2]:  # purchasable choice bundle
+        return PURCHASABLE_CHOICE_BUNDLE
+    # purchasable course choice bundle
+    elif mimeType == PURCHASABLE_COURSE_CHOICE_BUNDLE[3]:
+        return PURCHASABLE_COURSE_CHOICE_BUNDLE
+    return None
+
 
 def from_delimited(value, delim=' '):
-	result = value.split(delim)
-	result = re.findall("[^\s]+", value) if len(result) <= 1 else result
-	return result
+    result = value.split(delim)
+    result = re.findall("[^\s]+", value) if len(result) <= 1 else result
+    return result
+
 
 def to_collection(items=None, factory=list, delim=' '):
-	result = None
-	if not items:
-		result = factory()
-	elif isinstance(items, factory):
-		result = items
-	elif isinstance(items, six.string_types):
-		result = factory(from_delimited(unicode(items), delim))
-	else:
-		result = factory(items)
-	return result
+    result = None
+    if not items:
+        result = factory()
+    elif isinstance(items, factory):
+        result = items
+    elif isinstance(items, six.string_types):
+        result = factory(from_delimited(unicode(items), delim))
+    else:
+        result = factory(items)
+    return result
+
 
 def to_set(items=None, delim=' '):
-	return to_collection(items, set, delim)
+    return to_collection(items, set, delim)
+
 
 def to_list(items=None, delim=' '):
-	return to_collection(items, list, delim)
+    return to_collection(items, list, delim)
+
 
 def to_frozenset(items=None, delim=' '):
-	return to_collection(items, frozenset, delim)
+    return to_collection(items, frozenset, delim)
+
 
 def copy_object(source, *args, **kwargs):
-	copier = ICopier(source, None)
-	if copier is not None:
-		result = copier(source, *args, **kwargs)
-	else:
-		result = source.copy(*args, **kwargs)
-	return result
+    copier = ICopier(source, None)
+    if copier is not None:
+        result = copier(source, *args, **kwargs)
+    else:
+        result = source.copy(*args, **kwargs)
+    return result
+
 
 # meta classes
 
+
 class MetaStoreObject(type):
 
-	def __new__(cls, name, bases, dct):
-		cls = type.__new__(cls, name, bases, dct)
-		ancestor = object
-		for ancestor in cls.mro():
-			if 'mimeType' in ancestor.__dict__:
-				break
-		if ancestor is not cls:
-			clazzname = b'.' + name.encode('ascii').lower()
-			cls.mime_type = cls.mimeType = STORE_MIME_BASE + clazzname
-			cls.parameters = dict()
-		return cls
+    def __new__(cls, name, bases, dct):
+        cls = type.__new__(cls, name, bases, dct)
+        ancestor = object
+        for ancestor in cls.mro():
+            if 'mimeType' in ancestor.__dict__:
+                break
+        if ancestor is not cls:
+            clazzname = b'.' + name.encode('ascii').lower()
+            cls.mime_type = cls.mimeType = STORE_MIME_BASE + clazzname
+            cls.parameters = dict()
+        return cls
