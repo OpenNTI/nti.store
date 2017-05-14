62341ee658b5a799de015652c8853214d147c238
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-14 22:24:57 +0000

    kill unicode_literals
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.store/trunk@112720 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/store/purchase_attempt.py
+++ src/nti/store/purchase_attempt.py
@@ -4,7 +4,7 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -443,5 +443,5 @@ from zope.deprecation import deprecated
 deprecated("EnrollmentPurchaseAttempt", "use proper course enrollment")
 @interface.implementer(IEnrollmentPurchaseAttempt)
 class EnrollmentPurchaseAttempt(PurchaseAttempt):
-    mime_type = mimeType = MIME_BASE + b'enrollmentpurchaseattempt'
+    mime_type = mimeType = MIME_BASE + 'enrollmentpurchaseattempt'
     Processor = FP(IEnrollmentPurchaseAttempt['Processor'])
