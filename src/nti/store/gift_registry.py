4fa8905f10d07051c4c9ec91a451eca2f81d6cea
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-16 20:01:58 +0000

    remove deprecation
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.store/trunk@112936 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/store/gift_registry.py
+++ src/nti/store/gift_registry.py
@@ -27,7 +27,7 @@ from persistent import Persistent
 
 from nti.containers.containers import CaseInsensitiveCheckingLastModifiedBTreeContainer
 
-from nti.dataserver.dicts import LastModifiedDict
+from nti.containers.dicts import LastModifiedDict
 
 from nti.externalization.interfaces import LocatedExternalList
 
@@ -51,125 +51,134 @@ from nti.store.utils import to_frozenset
 
 from nti.zope_catalog.catalog import ResultSet
 
+
 deprecated('UserGiftHistory', 'Use new gift purchase storage')
 @interface.implementer(IUserGiftHistory)
 class UserGiftHistory(Contained, Persistent):
-	pass
+    pass
+
 
 class GiftRecordMap(LastModifiedDict, Contained):
 
-	def __init__(self, username=None):
-		LastModifiedDict.__init__(self)
-		self.username = username
+    def __init__(self, username=None):
+        LastModifiedDict.__init__(self)
+        self.username = username
+
 
 @interface.implementer(IGiftRegistry)
 class GiftRegistry(CaseInsensitiveCheckingLastModifiedBTreeContainer):
 
-	def __init__(self):
-		super(GiftRegistry, self).__init__()
+    def __init__(self):
+        super(GiftRegistry, self).__init__()
 
-	@property
-	def Items(self):
-		return dict(self)
+    @property
+    def Items(self):
+        return dict(self)
 
-	@property
-	def intids(self):
-		result = component.getUtility(IIntIds)
-		return result
+    @property
+    def intids(self):
+        result = component.getUtility(IIntIds)
+        return result
 
-	def register_purchase(self, username, purchase):
-		assert IGiftPurchaseAttempt.providedBy(purchase)
+    def register_purchase(self, username, purchase):
+        assert IGiftPurchaseAttempt.providedBy(purchase)
 
-		try:
-			index = self[username]
-		except KeyError:
-			index = GiftRecordMap(username)
-			self[username] = index
+        try:
+            index = self[username]
+        except KeyError:
+            index = GiftRecordMap(username)
+            self[username] = index
 
-		# locate before firing events
-		locate(purchase, index)
-		# add to connection and fire events
-		IConnection(self).add(purchase)
-		lifecycleevent.created(purchase)
-		lifecycleevent.added(purchase)  # get an iid
-		# now we can get an OID/NTIID and set creator
-		purchase.creator = username
-		purchase.id = unicode(to_external_ntiid_oid(purchase))
-		index[purchase.id] = purchase
-		return purchase
-	add = append = add_purchase = register_purchase
+        # locate before firing events
+        locate(purchase, index)
+        # add to connection and fire events
+        IConnection(self).add(purchase)
+        lifecycleevent.created(purchase)
+        lifecycleevent.added(purchase)  # get an iid
+        # now we can get an OID/NTIID and set creator
+        purchase.creator = username
+        purchase.id = unicode(to_external_ntiid_oid(purchase))
+        index[purchase.id] = purchase
+        return purchase
+    add = append = add_purchase = register_purchase
 
-	def remove_purchase(self, username, purchase):
-		if username in self:
-			index = self[username]
-			pid = getattr(purchase, 'id', purchase)
-			if index.pop(pid) is not None:
-				lifecycleevent.removed(purchase)  # remove iid
-				locate(purchase, None)
-				return True
-		return False
-	remove = remove_purchase
+    def remove_purchase(self, username, purchase):
+        if username in self:
+            index = self[username]
+            pid = getattr(purchase, 'id', purchase)
+            if index.pop(pid) is not None:
+                lifecycleevent.removed(purchase)  # remove iid
+                locate(purchase, None)
+                return True
+        return False
+    remove = remove_purchase
 
-	def get_purchases(self, username):
-		try:
-			index = self[username]
-			return list(index.values())
-		except KeyError:
-			return ()
+    def get_purchases(self, username):
+        try:
+            index = self[username]
+            return list(index.values())
+        except KeyError:
+            return ()
 
-	def get_pending_purchases(self, username, items=None):
-		result = []
-		try:
-			index = self[username]
-			items = to_frozenset(items) if items else None
-			for p in index.values():
-				if 		(p.is_pending() or p.is_unknown()) \
-					and (not items or to_frozenset(p.Items).intersection(items)):
-					result.append(p)
-		except KeyError:
-			pass
-		return result or ()
+    def get_pending_purchases(self, username, items=None):
+        result = []
+        try:
+            index = self[username]
+            items = to_frozenset(items) if items else None
+            for p in index.values():
+                if      (p.is_pending() or p.is_unknown()) \
+                    and (not items or to_frozenset(p.Items).intersection(items)):
+                    result.append(p)
+        except KeyError:
+            pass
+        return result or ()
 
-	def get_purchase_history(self, username, start_time=None, end_time=None):
-		result = get_gift_purchase_history(username, start_time, end_time)
-		return result
+    def get_purchase_history(self, username, start_time=None, end_time=None):
+        result = get_gift_purchase_history(username, start_time, end_time)
+        return result
+
 
 def get_gift_registry(registry=component):
-	result = registry.getUtility(IGiftRegistry)
-	return result
+    result = registry.getUtility(IGiftRegistry)
+    return result
+
 
 def get_gift_purchase_attempt(purchase_id, username=None):
-	result = find_object_with_ntiid(purchase_id)
-	result = result if IGiftPurchaseAttempt.providedBy(result) else None
-	if result is not None and username:
-		result = None if result.creator != username else result
-	return result
+    result = find_object_with_ntiid(purchase_id)
+    result = result if IGiftPurchaseAttempt.providedBy(result) else None
+    if result is not None and username:
+        result = None if result.creator != username else result
+    return result
+
 
 def remove_gift_purchase_attempt(purchase_id, username):
-	purchase = get_gift_purchase_attempt(purchase_id, username)
-	if purchase is not None and username:
-		registry = get_gift_registry()
-		result = registry.remove_purchase(username, purchase)
-		return result
-	return False
+    purchase = get_gift_purchase_attempt(purchase_id, username)
+    if purchase is not None and username:
+        registry = get_gift_registry()
+        result = registry.remove_purchase(username, purchase)
+        return result
+    return False
+
 
 def get_gift_pending_purchases(username, items=None):
-	registry = get_gift_registry()
-	result = registry.get_pending_purchases(username, items)
-	return result
+    registry = get_gift_registry()
+    result = registry.get_pending_purchases(username, items)
+    return result
+
 
 def get_gift_purchase_history(username, start_time=None, end_time=None, catalog=None):
-	intids = component.getUtility(IIntIds)
-	catalog = get_purchase_catalog() if catalog is None else catalog
-	query = {
-		IX_CREATOR: {'any_of': (username,)},
-		IX_CREATEDTIME: {'between': (start_time, end_time)},
-		IX_MIMETYPE: {'any_of': GIFT_PURCHASE_ATTEMPT_MIME_TYPES}
-	}
-	doc_ids = catalog.apply(query) or ()
-	return LocatedExternalList(ResultSet(doc_ids, intids, True))
+    intids = component.getUtility(IIntIds)
+    catalog = get_purchase_catalog() if catalog is None else catalog
+    query = {
+        IX_CREATOR: {'any_of': (username,)},
+        IX_CREATEDTIME: {'between': (start_time, end_time)},
+        IX_MIMETYPE: {'any_of': GIFT_PURCHASE_ATTEMPT_MIME_TYPES}
+    }
+    doc_ids = catalog.apply(query) or ()
+    return LocatedExternalList(ResultSet(doc_ids, intids, True))
+
 
 def register_gift_purchase_attempt(username, purchase):
-	registry = get_gift_registry()
-	result = registry.register_purchase(username, purchase)
-	return result.id
+    registry = get_gift_registry()
+    result = registry.register_purchase(username, purchase)
+    return result.id
