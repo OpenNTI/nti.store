7fdc12872876889461461a99d741e0221b9fd995
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-17 00:14:24 +0000

    enable generation 11 to update the user gift registry container
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.store/trunk@112939 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/store/generations/install.py
+++ src/nti/store/generations/install.py
@@ -11,7 +11,7 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-generation = 10
+generation = 11
 
 from zope.generations.generations import SchemaManager
 
