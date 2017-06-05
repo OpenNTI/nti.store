909bc44bc3e373a84f8b3e3240f21cef13702275
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-06-05 20:30:05 +0000

    test index
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.store/trunk@114298 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/store/tests/test_purchase_index.py
+++ src/nti/store/tests/test_purchase_index.py
@@ -13,12 +13,17 @@ from hamcrest import is_not
 from hamcrest import has_length
 from hamcrest import assert_that
 
+from nti.testing.matchers import is_empty
+
 import unittest
 
 from nti.store.purchase_index import StoreCatalog
-from nti.store.purchase_index import get_purchase_catalog
+from nti.store.purchase_index import PurchasableCatalog
 
-from nti.dataserver.tests.mock_dataserver import WithMockDSTrans
+from nti.store.purchase_index import create_purchase_catalog
+from nti.store.purchase_index import create_purchasable_catalog
+
+from nti.store.store import get_purchasable
 
 from nti.store.tests import SharedConfiguringTestLayer
 
@@ -27,9 +32,30 @@ class TestPurchaseIndex(unittest.TestCase):
 
     layer = SharedConfiguringTestLayer
 
-    @WithMockDSTrans
     def test_purchase_catalog(self):
-        catalog = get_purchase_catalog()
+        catalog = create_purchase_catalog()
         assert_that(catalog, is_not(none()))
         assert_that(catalog, has_length(8))
         assert_that(isinstance(catalog, StoreCatalog), is_(True))
+
+    def test_purchasable_catalog(self):
+        catalog = create_purchasable_catalog()
+        assert_that(catalog, is_not(none()))
+        assert_that(catalog, has_length(10))
+        assert_that(isinstance(catalog, PurchasableCatalog), is_(True))
+
+        ntiid = 'tag:nextthought.com,2011-10:NextThought-purchasable-HelpCenter'
+        purchasable = get_purchasable(ntiid)
+        purchasable.Label = u'HelpCenter'
+        catalog.index_doc(1, purchasable)
+
+        for query in (
+                {'ntiid': {'any_of': (ntiid,)}},
+                {'public': {'any_of': (True,)}},
+                {'currency': {'any_of': ('USD',)}},
+                {'label': {'any_of': ('HelpCenter',)}},
+                {'provider': {'any_of': ('NTI-TEST',)}},
+                {'mimeType': {'any_of': ('application/vnd.nextthought.store.purchasable',)}},
+                {'items': {'any_of': ('tag:nextthought.com,2011-10:NextThought-HTML-NextThoughtHelpCenter.nextthought_help_center',)}}):
+            results = catalog.apply(query) or ()
+            assert_that(results, is_not(is_empty()))
